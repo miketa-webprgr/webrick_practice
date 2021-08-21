ca8c6d0b4131163edcb7aafaabaa167ad4af594f
miketa-webprgr <ksuedomi91@gmail.com>
2021-08-21 15:50:09 +0900

    webrickにてreceive_formで受け付けるようにした

--- webrick.rb
+++ webrick.rb
@@ -12,42 +12,38 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
-server.mount_proc("/form_get") do |req, res|
-  name = req.query['get_name']
-  age = req.query['get_age']
-  color = req.query['get_color']
+server.mount_proc("/receive_form") do |req, res|
+  http_method = req.request_method
+  if http_method == "GET"
+      name = req.query['get_name']
+      age = req.query['get_age']
+      color = req.query['get_color']
 
-  body = "<html><meta charset='utf-8'><body>\n"
-  
-  body += "#{req.query}"
-  body += "<p>名前：#{name.force_encoding("utf-8")}</p>"
-  body += "<p>年齢：#{age.to_s}</p>"
-  body += "<p style = 'background-color:#{color.to_s}'>好きな色: #{color.to_s}</div>"
+      res.status = 200
+      res['Content-Type'] = 'text/html'
+      res.body = make_http(req.query, http_method, name, age, color)
+  elsif http_method == "POST"
+      name = req.query['post_name']
+      age = req.query['post_age']
+      color = req.query['post_color']
 
-  body += "</body></html>\n"
-
-  res.status = 200
-  res['Content-Type'] = 'text/html'
-  res.body = body
+      res.status = 200
+      res['Content-Type'] = 'text/html'
+      res.body = make_http(req.query, http_method, name, age, color)
+   end
 end
 
-server.mount_proc("/form_post") do |req, res|
-  name = req.query['post_name']
-  age = req.query['post_age']
-  color = req.query['post_color']
-
+def make_http(req_query, http_method, name, age, color)
   body = "<html><meta charset='utf-8'><body>\n"
   
-  body += "#{req.query}"
+  body += "<p>HTTPメソッドは#{http_method.to_s}です</p>"
+  body += "#{req_query}"
   body += "<p>名前：#{name.force_encoding("utf-8")}</p>"
   body += "<p>年齢：#{age.to_s}</p>"
   body += "<p style = 'background-color:#{color.to_s}'>好きな色: #{color.to_s}</div>"
-
   body += "</body></html>\n"
-
-  res.status = 200
-  res['Content-Type'] = 'text/html'
-  res.body = body
+  
+  body
 end
 
 
